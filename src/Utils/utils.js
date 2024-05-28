ea5c2a793bf687500bdcc1b533a556c881e94b72
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-05-29 05:58:21 +0600

    Crate menu base and objects

---
+++ src/Utils/utils.js
@@ -0,0 +1,7 @@
+export const routes = [
+  { id: 1, path: "/", name: "Home", component: "Home" },
+  { id: 2, path: "/about", name: "About", component: "About" },
+  { id: 3, path: "/contact", name: "Contact", component: "Contact" },
+  { id: 4, path: "/services", name: "Services", component: "Services" },
+  { id: 5, path: "/profile/:username", name: "Profile", component: "Profile" },
+];
