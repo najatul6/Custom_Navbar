07102d7ccc59e9c457abc73881fd4426087c531d
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-10-10 04:02:16 +0600

    Update utils menu

--- src/Utils/utils.js
+++ src/Utils/utils.js
@@ -6,9 +6,66 @@ export const routes = [
   { id: 5, path: "/profile/:username", name: "Profile", component: "Profile" },
 ];
 export const subRoutes = [
-  { id: 1, path: "/digitalMarketing", name: "Digital Marketing", component: "Digital Marketing" },
-  { id: 2, path: "/graphicDesign", name: "Graphic Design", component: "Graphic Design" },
-  { id: 3, path: "/webDevelopment", name: "Web Development", component: "Web Development" },
-  { id: 4, path: "/ai", name: "Artificial intelligence", component: "Artificial intelligence" },
+  {
+    id: 1,
+    path: "/digitalMarketing",
+    name: "Digital Marketing",
+    component: "Digital Marketing",
+  },
+  {
+    id: 2,
+    path: "/graphicDesign",
+    name: "Graphic Design",
+    component: "Graphic Design",
+  },
+  {
+    id: 3,
+    path: "/webDevelopment",
+    name: "Web Development",
+    component: "Web Development",
+  },
+  {
+    id: 4,
+    path: "/ai",
+    name: "Artificial intelligence",
+    component: "Artificial intelligence",
+  },
 ];
 
+export const NavMenu = [
+  {
+    id: "uerj4_43",
+    name: "Home",
+    link: "/",
+  },
+  {
+    id: "uerj4_44",
+    name: "Services",
+    link: "#",
+  },
+  {
+    id: "uerj4_45",
+    name: "Programs",
+    link: "#",
+  },
+  {
+    id: "uerj4_46",
+    name: "About Us",
+    link: "#",
+  },
+  {
+    id: "uerj4_47",
+    name: "Contact Us",
+    link: "#",
+  },
+  {
+    id: "uerj4_48",
+    name: "Blog",
+    link: "#",
+  },
+  {
+    id: "uerj4_49",
+    name: "Pricing",
+    link: "#",
+  },
+];
